c392b07adb2cfcbb0fa789dd4b0f9da6bec18a01
Evgenii <job4jstudy@yandex.ru>
2021-09-01 12:14:35 +1000

    to String

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,6 +1,7 @@
 package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * @author Evgenii Shegai
@@ -13,6 +14,7 @@ public class Item {
     private int id;
     private String name;
     private LocalDateTime created = LocalDateTime.now();
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
 
     public Item() {
     }
@@ -44,5 +46,15 @@ public class Item {
 
     public LocalDateTime getCreated() {
         return created;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" + "id=" + id + ", name='" + name + '\''
+                + ", created=" + created.format(FORMATTER) + '}';
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Item());
     }
 }
