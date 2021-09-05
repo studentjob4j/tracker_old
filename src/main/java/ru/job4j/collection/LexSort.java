479c564eb1d88a4774b1f755ab87b87acfb0632d
Evgenii <job4jstudy@yandex.ru>
2021-09-05 16:30:40 +1000

    Java Doc

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -3,12 +3,21 @@ package ru.job4j.collection;
 import java.util.Comparator;
 
 /**
+ * Класс сортирует строки по номерам
  * @author Evgenii Shegai
  * @since 05.09.2021
  * @version 1.0
  */
 
 public class LexSort implements Comparator<String> {
+
+
+    /**
+     *Метод принимает на вход 2 стринга и сравнивает их первые символы
+     * @param o1
+     * @param o2
+     * @return число результат сравненеия
+     */
 
     @Override
     public int compare(String o1, String o2) {
